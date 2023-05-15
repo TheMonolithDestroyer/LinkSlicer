3a6fc52659078616be60f791bfa7aef6ec2e3f60
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2023-05-15 19:00:32 +0600

    Created src, tests folders. Created Link.Slicer.Domain classLib project.

---
+++ Link.Slicer.Domain/Common/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace Link.Slicer.Domain.Common
+{
+    /// <summary>
+    /// Базовая сущность
+    /// </summary>
+    public class BaseEntity
+    {
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTimeOffset CreatedAt { get; set; }
+        /// <summary>
+        /// Дата обновления
+        /// </summary>
+        public DateTimeOffset UpdatedAt { get; set; }
+        /// <summary>
+        /// Дата удаления
+        /// </summary>
+        public DateTimeOffset? DeletedAt { get; set; }
+    }
+}
---
+++ Link.Slicer.Domain/Entities/Url.cs
@@ -0,0 +1,39 @@
+﻿using Link.Slicer.Domain.Common;
+
+namespace Link.Slicer.Domain.Entities
+{
+    /// <summary>
+    /// Урл
+    /// </summary>
+    public class Url : BaseEntity
+    {
+        /// <summary>
+        /// Первичный ключ
+        /// </summary>
+        public Guid UrlId { get; set; }
+        /// <summary>
+        /// Ключ сокращения
+        /// </summary>
+        public string Shortening { get; set; }
+        /// <summary>
+        /// Срок годности
+        /// </summary>
+        public long? ExpiresIn { get; set; }
+        /// <summary>
+        /// Протокол
+        /// </summary>
+        public string Protocol { get; set; }
+        /// <summary>
+        /// Домен
+        /// </summary>
+        public string DomainName { get; set; }
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Комментарий
+        /// </summary>
+        public string Comment { get; set; }
+    }
+}
